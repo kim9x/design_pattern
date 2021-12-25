cbf3be1ba3dd633f485178ca44dd65808a1fda16
Kim9xd <kim9xd@gmail.com>
2021-12-25 17:51:10 +0900

    * 객체 생성 관련 디자인 패턴    - 싱글톤 패턴 1부 - 싱글톤 패턴을 가장 단순히 구현하는 방법    - 싱글톤 패턴 2부 - 멀티 쓰레드 환경에서 안전하게 구현하는 방법

---
+++ src/main/java/me/pulpury/designpatterns/DesignPatternsPulpuryApplication.java
@@ -0,0 +1,15 @@
+package me.pulpury.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsPulpuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsPulpuryApplication.class, args);
+		
+		
+	}
+
+}
