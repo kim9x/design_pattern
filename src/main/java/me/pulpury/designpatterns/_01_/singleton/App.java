da4d16766a9a689b9ead9bc8a4405452e10f128e
Kim9xd <kim9xd@gmail.com>
2021-12-27 15:09:06 +0900

    * 객체 생성 관련 디자인 패턴    - 싱글톤 패턴 4부 - 안전하고 단순하게 구현하는 방법

--- src/main/java/me/pulpury/designpatterns/_01_/singleton/App.java
+++ src/main/java/me/pulpury/designpatterns/_01_/singleton/App.java
@@ -1,41 +1,15 @@
 package me.pulpury.designpatterns._01_.singleton;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.lang.reflect.Constructor;
 
 public class App {
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-		Settings settings = Settings.getInstance();
+		
+		// 아래처럼 사용하면 reflection에도 안전하다. 
+		Settings settings = Settings.INSTANCE;
 //		Settings settings1 = Settings.getInstance();
-		Settings settings1 = null;
-		
-		
-		// 파일 등에 저장할 때 사용하는 직렬화
-		// 불러올 때 사용한는 역직렬화 사용 시
-		// 동일 객체가 아닐 수 있다.
-		try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
-			out.writeObject(settings);
-		}
-		
-		try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
-			settings1 = (Settings) in.readObject();
-		}
-		
-		System.out.println(settings == settings1);
-		
-		// 아래처럼 리플렉션 사용 시 new로 해서 사용하는 것과 같다.
-		// Thread safe가 깨진다.
-//		Constructor<Settings> constructor = Settings.class.getDeclaredConstructors();
-//		constructor.setAccessible(true);
-//		Settings settings1 = constructor.newInstance();
 //		
 //		System.out.println(settings == settings1);
 	}
--- src/main/java/me/pulpury/designpatterns/_01_/singleton/Settings.java
+++ src/main/java/me/pulpury/designpatterns/_01_/singleton/Settings.java
@@ -2,69 +2,23 @@ package me.pulpury.designpatterns._01_.singleton;
 
 import java.io.Serializable;
 
-public class Settings implements Serializable {
+
+// enum을 이용해서 singleton 만들기
+public enum Settings {
 	
-	// 방법 3 사용 시 
-	// java 1.5 버전 이상부턴
-	// volatile 키워드를 사용해줘야
-	// double checked locking 기법 적용 가능
-//	private static volatile Settings instance;
+	INSTANCE;
 	
-	// Thread safe 방법2
-	// 이른 초기화(eager initialization) 사용하기
-	// final 변수는 대문자로 사용하는 것이 규약이다.
-	// 무조건 초기화를 해주는 과정이 단점이 될 수 있다.
-	// 비용을 많이 들여 초기화를 해놨는데
-	// 사용하지 않을 수도 있기 때문.
-//	private static final Settings INSTANCE = new Settings();
-	
-	private Settings() {}
-	
-	// Thread safe 방법4
-	// static inner 클래스 사용하기
-	private static class SettingsHolder {
-		private static final Settings INSTANCE = new Settings();
+	private Settings() {
 	}
-	
-	// Thread safe 방법1
-	// synchronized 키워드 사용
-	// nut 성능이 떨어질 수 있다.
-	public static synchronized Settings getInstance() {
-		
-		// 방법 2 사용 시 INSTANCE를 바로 반환해주면 된다.
-//		if (instance == null) {
-//			instance = new Settings();
-//		}
-//		
-//		return instance;
-		
-		// 아래가 방법 2 사용 시 바로 반환하는 return 
-//		return INSTANCE;
-		
-		// Thread safe 방법 3
-		// double checked locking 사용하기
-//		if (instance == null) {
-//			synchronized (Settings.class) {
-//				if (instance == null) {
-//					instance = new Settings();
-//				}
-//				
-//			}
-//		}
-//		return instance;
-		
-		// Tread safe 방법4
-		// 가장 권장하는 방법 중에 하나!
-		return SettingsHolder.INSTANCE;
-		
-		
+
+	private Integer number;
+
+	public Integer getNumber() {
+		return number;
 	}
-	
-	// 역직렬화 할 때
-	// 아래 readResolve를 사용하므로
-	// 직열화/역직열화는 해결이 가능하다.
-	protected Object readResolve() {
-		return getInstance();
+
+	public void setNumber(Integer number) {
+		this.number = number;
 	}
 
 }
