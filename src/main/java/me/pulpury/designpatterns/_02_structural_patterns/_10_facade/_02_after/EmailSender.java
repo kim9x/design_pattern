cef4ffef0da6befa280108889f28f52a3e63aef8
Kim9xd <kim9xd@gmail.com>
2021-12-31 03:41:38 +0900

    * 구조 관련 디자인 패턴    - 퍼사드 패턴 2부 - 패턴 적용하기

---
+++ src/main/java/me/pulpury/designpatterns/_02_structural_patterns/_10_facade/_02_after/EmailSender.java
@@ -0,0 +1,40 @@
+package me.pulpury.designpatterns._02_structural_patterns._10_facade._02_after;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailSender {
+	
+	EmailSettings emailSettings;
+
+	public EmailSender(EmailSettings emailSettings) {
+		this.emailSettings = emailSettings;
+	}
+	
+	public void sendEamil(EmailMessage emailMessage) {
+
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", emailSettings.getHost());
+
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(emailMessage.getFrom()));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailMessage.getTo()));
+            message.setSubject(emailMessage.getSubject());
+            message.setText(emailMessage.getText());
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+	}
+
+}
