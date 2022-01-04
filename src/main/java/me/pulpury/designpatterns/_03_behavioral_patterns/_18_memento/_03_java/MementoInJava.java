d21bcfe51bbb440091054ef0677796871332abde
Kim9xd <kim9xd@gmail.com>
2022-01-04 14:04:58 +0900

    * 행동 관련 디자인 패턴    - 메멘토 패턴 4부 - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/me/pulpury/designpatterns/_03_behavioral_patterns/_18_memento/_03_java/MementoInJava.java
@@ -0,0 +1,38 @@
+package me.pulpury.designpatterns._03_behavioral_patterns._18_memento._03_java;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import me.pulpury.designpatterns._03_behavioral_patterns._18_memento._02_after.Game;
+
+public class MementoInJava {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        // TODO Serializable
+        Game game = new Game();
+        game.setRedTeamScore(10);
+        game.setBlueTeamScore(20);
+
+        // TODO 직렬화
+        try(FileOutputStream fileOut = new FileOutputStream("GameSave.hex");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut))
+        {
+            out.writeObject(game);
+        }
+
+        game.setBlueTeamScore(25);
+        game.setRedTeamScore(15);
+
+        // TODO 역직렬화
+        try(FileInputStream fileIn = new FileInputStream("GameSave.hex");
+            ObjectInputStream in = new ObjectInputStream(fileIn))
+        {
+            game = (Game) in.readObject();
+            System.out.println(game.getBlueTeamScore());
+            System.out.println(game.getRedTeamScore());
+        }
+    }
+}
